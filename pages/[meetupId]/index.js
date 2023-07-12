625f10510593489508ad60ff13261db18b3507e2
Masoumeh Rezai <you@example.com>
2023-07-13 00:33:51 +0430

    add meetup Item

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const MeetupDeatila = () => {
+    return (
+        <Fragment>
+            <img src=''  />
+
+        </Fragment>
+    )
+}
+export default MeetupDeatila;
