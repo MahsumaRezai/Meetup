86e8f2009163e31ccfb43a40c6a9c6c533c1bf96
Masoumeh Rezai <you@example.com>
2023-07-13 00:48:40 +0430

    last commit for today wen

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -3,7 +3,10 @@ import { Fragment } from "react";
 const MeetupDeatila = () => {
     return (
         <Fragment>
-            <img src=''  />
+            <img src="" />
+            <h1>this is first meetup</h1>
+            <address>some address in heart city</address>
+            <p>The meetup infromaton</p>
 
         </Fragment>
     )
